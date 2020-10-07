d0bfbc67572bb284054eff7daffaf2f40c10a5f6
james <james.slycat@gmail.com>
2020-10-07 00:43:07 -0400

    maploader handles newlines differently/maybe better

--- WarzonePart1/WarzonePart1/MapLoader.cpp
+++ WarzonePart1/WarzonePart1/MapLoader.cpp
@@ -33,8 +33,8 @@ Map *MapLoader::createMap()
     string word;
     string line;
 
-    Territory* current;
-    
+    Territory *current;
+
     string name;
     int territory_id;
     int continent_id;
@@ -62,77 +62,72 @@ Map *MapLoader::createMap()
             mode = border;
         }
 
+        // this should be done ONLY if tag was not read
+
         tokens = vector<string>();
         iss = istringstream(line);
 
         while (iss >> word)
             tokens.push_back(word);
 
-        // consume newlines
-
-        if (&tokens[0] == NULL)
-        {
-            cout << "this is a newline" << endl;
-            mode = other;
-        }
-
-        // this should be done ONLY if tag was not read
-
-        switch (mode)
+        // skip newlines
+        if (line.size() != 0)
         {
 
-        case continent:
-
-            continent_id = continent_count++;
-            name = tokens[0];
-
-            continents.push_back(new Continent(continent_id, name));
-            cout << *continents.back() << endl;
-            break;
-
-        case territory:
-
-            territory_id = stoi(tokens[0]) - 1;
-            continent_id = stoi(tokens[2]) - 1;
-
-            name = tokens[1];
-
-            
-
-            territories.push_back(new Territory(territory_id, name, continent_id));
-            current = territories.back();
-            cout << current->getId() << endl;
-            cout << *territories.back() << endl;
-            break;
-
-        case border:
-
-            // first number is the territory to add borders to
-            // 3 2 3 11 ---> 3 is current_id, [2, 3, 11] are borders
-            int current_id = stoi(tokens[0]) - 1;
-            Territory *territory = territories[current_id];
-
-            // the rest are borders
-            cout << "Here are some borders of " << territory->getTerritoryName() << " " << current_id << endl;
-            cout << "------------------------" << endl;
-            tokens.erase(tokens.begin());
-            for (int i = 0; i < tokens.size(); ++i)
+            switch (mode)
             {
-                int adjacent_id = stoi(tokens[i]) - 1;
-                territory->addBorder(territories[adjacent_id]);
-                cout << territories[adjacent_id]->getTerritoryName() << endl;
-            }
-            break;
-        };
+
+            case continent:
+
+                continent_id = continent_count++;
+                name = tokens[0];
+
+                continents.push_back(new Continent(continent_id, name));
+                cout << *continents.back() << endl;
+                break;
+
+            case territory:
+
+                territory_id = stoi(tokens[0]) - 1;
+                continent_id = stoi(tokens[2]) - 1;
+
+                name = tokens[1];
+
+                territories.push_back(new Territory(territory_id, name, continent_id));
+                current = territories.back();
+                cout << current->getId() << endl;
+                cout << *territories.back() << endl;
+                break;
+
+            case border:
+
+                // first number is the territory to add borders to
+                // 3 2 3 11 ---> 3 is current_id, [2, 3, 11] are borders
+                int current_id = stoi(tokens[0]) - 1;
+                Territory *territory = territories[current_id];
+
+                // the rest are borders
+                cout << "Here are some borders of " << territory->getTerritoryName() << " " << current_id << endl;
+                cout << "------------------------" << endl;
+                tokens.erase(tokens.begin());
+                for (int i = 0; i < tokens.size(); ++i)
+                {
+                    int adjacent_id = stoi(tokens[i]) - 1;
+                    territory->addBorder(territories[adjacent_id]);
+                    cout << territories[adjacent_id]->getTerritoryName() << endl;
+                }
+                break;
+            };
+        }
     }
 
-    
-
-    for (auto c : continents) {
+    for (auto c : continents)
+    {
         map->addContinent(c);
     }
 
-    for (auto t : territories) {
+    for (auto t : territories)
+    {
         map->addTerritory(t);
     }
 
@@ -144,6 +139,6 @@ Map *MapLoader::createMap()
 
 // Add readline method that will leave if tag is read
 
-MapLoader::MapLoader(string fileName):_fileName(fileName)
+MapLoader::MapLoader(string fileName) : _fileName(fileName)
 {
 }
