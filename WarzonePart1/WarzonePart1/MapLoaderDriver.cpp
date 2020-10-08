0344599d6789328335cb81afcd79db44ed8ea179
Noah Freger <noahf303@gmail.com>
2020-10-08 12:47:28 -0400

    fixed player driver issues

--- WarzonePart1/WarzonePart1/MapLoaderDriver.cpp
+++ WarzonePart1/WarzonePart1/MapLoaderDriver.cpp
@@ -1,9 +1,9 @@
-#include "MapLoader.h"
-
-int main()
-
-{   MapLoader loader = MapLoader("sw_baltic.map");
-    Map* map = loader.createMap();
-    map->validate();
-    delete map;
-}
+//#include "MapLoader.h"
+//
+//int main()
+//
+//{   MapLoader loader = MapLoader("sw_baltic.map");
+//    Map* map = loader.createMap();
+//    map->validate();
+//    delete map;
+//}
