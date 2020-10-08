592ad4c7232c4247607ff3cd651dea699a3f62ed
Noah Freger <noahf303@gmail.com>
2020-10-08 00:35:56 -0400

    removed comment

--- WarzonePart1/WarzonePart1/MapDriver.cpp
+++ WarzonePart1/WarzonePart1/MapDriver.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include "Map.h"
 using namespace std;
-/*
+
 int main()
 {
 	cout << "creating the map" << endl;
@@ -56,4 +56,4 @@ int main()
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file */
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file 
