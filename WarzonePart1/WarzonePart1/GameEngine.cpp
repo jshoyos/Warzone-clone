1fd7e33a5615cba3eb3562e5283fc994d00f789b
Juan Hoyos <juanse.jsh@gmail.com>
2020-11-09 14:56:46 -0500

    started part 1

--- WarzonePart1/WarzonePart1/GameEngine.cpp
+++ WarzonePart1/WarzonePart1/GameEngine.cpp
@@ -1,1 +1,17 @@
 #include "GameEngine.h"
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+
+const string GameStart::PATH = "Maps/";
+Map GameStart::selectMap()
+{
+    for (auto& entry : fs::directory_iterator(PATH)) {
+
+    }
+    return Map();
+}
+
+vector<Player*> GameStart::createPlayers(int)
+{
+    return vector<Player*>();
+}
--- WarzonePart1/WarzonePart1/GameEngine.h
+++ WarzonePart1/WarzonePart1/GameEngine.h
@@ -1,3 +1,19 @@
-#pragma once
+#include "MapLoader.h"
+#include <vector>
+#include <string.h>
 
+#pragma once
+class GameStart {
+private:
+	const static string PATH;
+	static vector<string> maps;
+	static int selectedOption;
+	static void displayMapOptions();
+
+public:
+#pragma region methods
+	static Map selectMap();
+	static vector<Player*> createPlayers(int);
+#pragma endregion
+};
 
