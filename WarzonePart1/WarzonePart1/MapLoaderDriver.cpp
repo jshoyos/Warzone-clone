ae4766004c91f2d3ab5b6619f6695a053b1fa7d6
Juan Hoyos <juanse.jsh@gmail.com>
2020-11-09 13:48:35 -0500

    added needed files for second delivery

--- WarzonePart1/WarzonePart1/MapLoaderDriver.cpp
+++ WarzonePart1/WarzonePart1/MapLoaderDriver.cpp
@@ -5,16 +5,16 @@ int main()
 {   
     // test good file
     MapLoader loader = MapLoader("sw_baltic.map");
-    Map* map1 = loader.createMap();
+    /*Map* map1 = loader.createMap();*/
     //map1->validate();
 
-    // // Test bad file (extension wrong)
-    // loader.setFileName("");
-    // Map* mapWrong1 = loader.createMap();
+     //// Test bad file (extension wrong)
+     //loader.setFileName("");
+     //Map* mapWrong1 = loader.createMap();
 
-    // // // test another bad file (invalid file)
-    // loader.setFileName(".map");
-    // Map* mapWrong2 = loader.createMap();
+     // // test another bad file (invalid file)
+     loader.setFileName(".map");
+     Map* mapWrong2 = loader.createMap();
 
     // test bad file (not all tokens)
     loader.setFileName("sw_baltic_bad.map");
