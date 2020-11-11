7405532ccc98141320aca2ade2472670a8086c01
Juan Hoyos <juanse.jsh@gmail.com>
2020-11-11 00:48:54 -0500

    set up the observer pattern

--- WarzonePart1/WarzonePart1/GameObservers.h
+++ WarzonePart1/WarzonePart1/GameObservers.h
@@ -1,5 +1,27 @@
+#include <vector>
+#include <string>
+using namespace std;
 #pragma once
-class GameObservers
-{
+class IObservable {
+public:
+	virtual void update() = 0;
 };
 
+class Publisher {
+private:
+	vector<reference_wrapper<IObservable>> subscribers;
+public:
+	void subscribe(IObservable&);
+	void nofityAll();
+	void unsubscribe(IObservable&);
+	bool isSubscribed(const IObservable&);
+};
+
+class PhaseObserver : IObservable {
+public:
+	void update() override;
+};
+
+class GameStatisticsObserver : IObservable {
+
+};
