d0ab8385cf78d99ee8deb5a153d90dc60a3762f2
noahfreger <50425626+noahfreger@users.noreply.github.com>
2020-12-02 18:44:04 -0500

    fixed nullptr error (#41)

--- WarzonePart1/WarzonePart1/AdapterPatternDriver.cpp
+++ WarzonePart1/WarzonePart1/AdapterPatternDriver.cpp
@@ -1,21 +1,21 @@
-//#include "GameEngine.h"
-//using namespace std;
-//namespace fs = std::filesystem;
-//
-//
-//// Driver to test the functionality of any "domination map" or "conquest map"
-//// Result: success in running both type of files using an Adapter strategy. 
-//
-//int main() {
-//
-//	// First, the method asks to select a map from a list of maps contained in a folder "Maps" 
-//	// Then the method checks what type of file it is (ie: either a domination or conquest map).
-//	// And based on that, we will either create a MapLoader or a ConquestFileReaderAdapter object  
-//	// in order to execute the method createmap()... 
-//	// This method will create continents, territories and borders. And finally, we validate the Map.
-//
-//	GameStart::selectMap();
-//
-//}
-//
-//
+#include "GameEngine.h"
+using namespace std;
+namespace fs = std::filesystem;
+
+
+// Driver to test the functionality of any "domination map" or "conquest map"
+// Result: success in running both type of files using an Adapter strategy. 
+
+int main() {
+
+	// First, the method asks to select a map from a list of maps contained in a folder "Maps" 
+	// Then the method checks what type of file it is (ie: either a domination or conquest map).
+	// And based on that, we will either create a MapLoader or a ConquestFileReaderAdapter object  
+	// in order to execute the method createmap()... 
+	// This method will create continents, territories and borders. And finally, we validate the Map.
+
+	GameStart::selectMap();
+
+}
+
+
